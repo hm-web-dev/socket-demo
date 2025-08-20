1b4b29e15649ee3dd060c9bfc68e12f1aeb3afa1
Ms. Feng <irene_feng@horacemann.org>
2025-08-20 15:05:29 -0400

    fix build paths for production

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '', // Set base to empty string for relative paths
   plugins: [vue()],
   resolve: {
     alias: {
